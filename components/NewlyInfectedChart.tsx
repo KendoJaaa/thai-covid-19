3c7fd2ec262b93068dec04f03568231b1db15292
kendo <kendo@flipay.co>
2020-03-21 23:37:39 +0700

    ok have 2 data

--- components/NewlyInfectedChart.tsx
+++ components/NewlyInfectedChart.tsx
@@ -2,159 +2,38 @@ import React, { FC } from "react";
 import { ResponsiveBar } from "@nivo/bar";
 import Chart from "./Chart";
 
-type Props = {};
+type Data = {
+  date: string,
+  confirmed: number
+  recovered: number
+  inhospital: number
+  died: number  
+  crosscheck: number  
+  new: number  
+  critical: number
+}
 
-const NewlyInfectedChart: FC<Props> = () => {
-  const data = [
-    {
-      country: "AD",
-      "hot dog": 44,
-      "hot dogColor": "hsl(43, 70%, 50%)",
-      burger: 115,
-      burgerColor: "hsl(359, 70%, 50%)",
-      sandwich: 158,
-      sandwichColor: "hsl(82, 70%, 50%)",
-      kebab: 1,
-      kebabColor: "hsl(317, 70%, 50%)",
-      fries: 61,
-      friesColor: "hsl(8, 70%, 50%)",
-      donut: 189,
-      donutColor: "hsl(100, 70%, 50%)"
-    },
-    {
-      country: "AE",
-      "hot dog": 107,
-      "hot dogColor": "hsl(328, 70%, 50%)",
-      burger: 47,
-      burgerColor: "hsl(138, 70%, 50%)",
-      sandwich: 19,
-      sandwichColor: "hsl(168, 70%, 50%)",
-      kebab: 151,
-      kebabColor: "hsl(129, 70%, 50%)",
-      fries: 186,
-      friesColor: "hsl(228, 70%, 50%)",
-      donut: 7,
-      donutColor: "hsl(100, 70%, 50%)"
-    },
-    {
-      country: "AF",
-      "hot dog": 48,
-      "hot dogColor": "hsl(351, 70%, 50%)",
-      burger: 82,
-      burgerColor: "hsl(272, 70%, 50%)",
-      sandwich: 79,
-      sandwichColor: "hsl(316, 70%, 50%)",
-      kebab: 58,
-      kebabColor: "hsl(73, 70%, 50%)",
-      fries: 134,
-      friesColor: "hsl(156, 70%, 50%)",
-      donut: 185,
-      donutColor: "hsl(236, 70%, 50%)"
-    },
-    {
-      country: "AG",
-      "hot dog": 36,
-      "hot dogColor": "hsl(189, 70%, 50%)",
-      burger: 134,
-      burgerColor: "hsl(108, 70%, 50%)",
-      sandwich: 185,
-      sandwichColor: "hsl(149, 70%, 50%)",
-      kebab: 200,
-      kebabColor: "hsl(333, 70%, 50%)",
-      fries: 134,
-      friesColor: "hsl(338, 70%, 50%)",
-      donut: 61,
-      donutColor: "hsl(333, 70%, 50%)"
-    },
-    {
-      country: "AI",
-      "hot dog": 152,
-      "hot dogColor": "hsl(54, 70%, 50%)",
-      burger: 120,
-      burgerColor: "hsl(270, 70%, 50%)",
-      sandwich: 105,
-      sandwichColor: "hsl(35, 70%, 50%)",
-      kebab: 87,
-      kebabColor: "hsl(130, 70%, 50%)",
-      fries: 86,
-      friesColor: "hsl(213, 70%, 50%)",
-      donut: 62,
-      donutColor: "hsl(253, 70%, 50%)"
-    },
-    {
-      country: "AL",
-      "hot dog": 31,
-      "hot dogColor": "hsl(221, 70%, 50%)",
-      burger: 32,
-      burgerColor: "hsl(299, 70%, 50%)",
-      sandwich: 69,
-      sandwichColor: "hsl(154, 70%, 50%)",
-      kebab: 38,
-      kebabColor: "hsl(14, 70%, 50%)",
-      fries: 25,
-      friesColor: "hsl(184, 70%, 50%)",
-      donut: 38,
-      donutColor: "hsl(283, 70%, 50%)"
-    },
-    {
-      country: "AM",
-      "hot dog": 9,
-      "hot dogColor": "hsl(100, 70%, 50%)",
-      burger: 169,
-      burgerColor: "hsl(7, 70%, 50%)",
-      sandwich: 89,
-      sandwichColor: "hsl(333, 70%, 50%)",
-      kebab: 76,
-      kebabColor: "hsl(25, 70%, 50%)",
-      fries: 83,
-      friesColor: "hsl(250, 70%, 50%)",
-      donut: 49,
-      donutColor: "hsl(272, 70%, 50%)"
-    }
-  ];
+type Propsja = {
+  dataSet: Data[]
+};
+
+function NewlyInfectedChart ({ dataSet }: Propsja) {
+
+  const formattedData = dataSet.map((data) => ({
+    date: data.date,
+    amount: data.new,
+    amountColor: "hsl(43, 70%, 50%)"
+  }))
+
   return (
     <Chart title="ผู้ติดเชื้อรายใหม่ รายวัน">
       <ResponsiveBar
-        data={data}
-        keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-        indexBy="country"
+        data={formattedData}
+        keys={["amount"]}
+        indexBy="date"
         margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
         padding={0.3}
         colors={{ scheme: "nivo" }}
-        defs={[
-          {
-            id: "dots",
-            type: "patternDots",
-            background: "inherit",
-            color: "#38bcb2",
-            size: 4,
-            padding: 1,
-            stagger: true
-          },
-          {
-            id: "lines",
-            type: "patternLines",
-            background: "inherit",
-            color: "#eed312",
-            rotation: -45,
-            lineWidth: 6,
-            spacing: 10
-          }
-        ]}
-        fill={[
-          {
-            match: {
-              id: "fries"
-            },
-            id: "dots"
-          },
-          {
-            match: {
-              id: "sandwich"
-            },
-            id: "lines"
-          }
-        ]}
         borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
         axisTop={null}
         axisRight={null}
@@ -162,7 +41,7 @@ const NewlyInfectedChart: FC<Props> = () => {
           tickSize: 5,
           tickPadding: 5,
           tickRotation: 0,
-          legend: "country",
+          legend: "วันที่",
           legendPosition: "middle",
           legendOffset: 32
         }}
@@ -170,7 +49,7 @@ const NewlyInfectedChart: FC<Props> = () => {
           tickSize: 5,
           tickPadding: 5,
           tickRotation: 0,
-          legend: "food",
+          legend: "จำนวน(คน)",
           legendPosition: "middle",
           legendOffset: -40
         }}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,39 @@
-
-import React from 'react'
-import { NextPage } from 'next';
-import axios from 'axios'
-import TotalCasesChart from '../components/TotalCasesChart'
+import React from "react";
+import { NextPage } from "next";
+import axios from "axios";
+import TotalCasesChart from "../components/TotalCasesChart";
+import NewlyInfectedChart from "../components/NewlyInfectedChart";
 
 type Data = {
-  date: string,
-  confirmed: number
-  recovered: number
-  inhospital: number
-  died: number  
-  crosscheck: number  
-  new: number  
-  critical: number
-}
+  date: string;
+  confirmed: number;
+  recovered: number;
+  inhospital: number;
+  died: number;
+  crosscheck: number;
+  new: number;
+  critical: number;
+};
 
-const Home: NextPage<{ dataSet: Data[] }> = ({ dataSet }) => (
-  <React.Fragment>
-    <h1>ข้อมูล เกี่ยวกับ corona</h1>
-    <TotalCasesChart 
-      dataSet={dataSet}
-    />
-  </React.Fragment>
-);
+const Home: NextPage<{ dataSet: Data[] }> = ({ dataSet }) => {
+  return (
+    <React.Fragment>
+      <h1>ข้อมูล เกี่ยวกับ corona</h1>
+      <TotalCasesChart dataSet={dataSet} />
+      <NewlyInfectedChart dataSet={dataSet} />
+    </React.Fragment>
+  );
+};
 
 Home.getInitialProps = async ({ req }) => {
   const request = axios.create({
-    baseURL: 'http://gsx2json.com/api?id=1XjHHZ9GiIH4ZiOrEP48WjHj_NHHSq9WgqOVatTqpfpU&sheet=1',
-    timeout: 1000,
+    baseURL:
+      "http://gsx2json.com/api?id=1XjHHZ9GiIH4ZiOrEP48WjHj_NHHSq9WgqOVatTqpfpU&sheet=1",
+    timeout: 1000
   });
-  const response = await request.get('')
-  const dataSet = response.data.rows
-  
+  const response = await request.get("");
+  const dataSet = response.data.rows;
+
   return { dataSet };
 };
 

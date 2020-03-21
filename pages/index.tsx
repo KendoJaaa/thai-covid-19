9821e903f5063e98e533ab31e639cc19c1746aac
kendo <kendo@flipay.co>
2020-03-21 21:11:00 +0700

    can fecth data from backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 
 import { NextPage } from 'next';
+import axios from 'axios'
 
 const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
   <h1>Hello world! - user agent: {userAgent}</h1>
 );
 
 Home.getInitialProps = async ({ req }) => {
+  console.log('kendo jaa ')
+  const request = axios.create({
+    baseURL: 'http://gsx2json.com/api?id=1XjHHZ9GiIH4ZiOrEP48WjHj_NHHSq9WgqOVatTqpfpU&sheet=1',
+    timeout: 1000,
+  });
+  const response = await request.get('')
+  console.log('kendo jaa', response.data)
+  
   const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
   return { userAgent };
 };
